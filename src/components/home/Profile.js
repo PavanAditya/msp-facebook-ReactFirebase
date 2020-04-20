02190bc78320e7becde7efbb8e73235e9aab050a
Pavanaditya Mylavarapu <M1050962@mindtree.com>
2020-04-20 15:37:10 +0530

    Profile Details Dynamic

--- src/components/home/Profile.js
+++ src/components/home/Profile.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Profile.css';
 import { Icon } from 'react-materialize';
+import { userRef, firebaseApp } from '../../firebase';
 
 const Profile = () => {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [contact, setContact] = useState('');
+    const [about, setAbout] = useState('');
+    const [uid, setUid] = useState('');
+
+
+    useEffect(() => {
+        firebaseApp.auth().onAuthStateChanged(function (data) {
+            if (data) {
+                setUid(data.uid);
+                console.log(data);
+                getName();
+            }
+        });
+        const getName = () => {
+            console.log(uid);
+            if (uid) {
+                userRef.child(uid).once('value', snapshot => {
+                    console.log(snapshot.val());
+                    setFirstName(snapshot.val()['firstName']);
+                    setLastName(snapshot.val()['lastName']);
+                    setContact(snapshot.val()['mobileNumber']);
+                    setAbout(snapshot.val()['about']);
+                    setLoading(false);
+                })
+            }
+        }
+    }, [uid]);
+
     return (
         <div className="outerBox m10 profileBox">
             <div style={{ display: 'flex', marginBottom: 10 }}>
@@ -16,20 +49,31 @@ const Profile = () => {
                         />
                     </div>
                 </div>
-                <div style={{ marginLeft: 10, flex: 1, fontSize: 'large' }}><b><i>Pavan Aditya M S</i></b></div>
+                <div style={{ marginLeft: 10, flex: 1, fontSize: 'large' }}>
+                    {loading ?
+                        <div className="loading post-name"></div> :
+                        <b><i>{firstName + ' ' + lastName}</i></b>
+                    }</div>
                 <div style={{ cursor: 'pointer' }}><Icon>edit</Icon></div>
             </div>
             <div style={{ borderTop: '1px solid lightgray' }}>
                 <div style={{ color: '#385898', marginTop: 10 }}>
-                    About me:
-                    </div>
+                    Contact:
+                </div>
                 <div style={{ fontSize: 12 }}>
-                    MEAN Stack Engineer at Mindtree Ltd.
-                    <br /> 20'Jul 97
-                    <br /> Novelist
-                    <br /> Movie Buff
-                    <br /> Cricket 😍🤩
-                    </div>
+                    {loading ?
+                        <div className="loading post-num"></div> :
+                        contact
+                    }
+                </div>
+                <div style={{ color: '#385898', marginTop: 10 }}>
+                    About me:
+                </div>
+                <div style={{ fontSize: 12 }}>
+                    {loading ?
+                        <div className="loading post-about"></div> :
+                        about}
+                </div>
             </div>
         </div>
     );
--- src/services/signIn.js
+++ src/services/signIn.js
@@ -2,6 +2,7 @@ import { firebaseApp, userRef } from '../firebase';
 
 export default ({ email, password }) => {
     firebaseApp.auth().signInWithEmailAndPassword(email, password).then((data) => {
+        localStorage.setItem('uid', data.user.uid);
         userRef.child(data.user.uid).once('value', (snapshot) => {
             console.log(snapshot.val());
             return snapshot.val();
